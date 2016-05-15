1991b42eb96f95c3bc9f4b9a6de359062b29f62a
Luc Awater <lucawater@gmail.com>
2016-05-15 15:15:34 +0200

    Tweak nav so it shows up on other pages

--- content/themes/elements/includes/nav.php
+++ content/themes/elements/includes/nav.php
@@ -1,44 +1,31 @@
 <nav>
   <ul>
     <?php
+    $homepage_ID = get_option( 'page_on_front' );
+
     if( have_posts() ):
       while( have_posts() ): the_post();
 
-        if( have_rows('page') ): $i_anchor = 1;
-          while( have_rows('page') ): the_row();
+        if( have_rows('page', $homepage_ID) ): $i_anchor = 1;
+          while( have_rows('page', $homepage_ID) ): the_row();
 
             if( get_row_layout() == 'text' ):
               $text_menu = get_sub_field( 'text_o_menu' );
               $text_menu_name = get_sub_field( 'text_o_menu_name' );
+              ( !is_front_page() ) ? $text_link = home_url() . '/#anchor-' . $i_anchor : $text_link = home_url() . '/#anchor-' . $i_anchor;
               if( $text_menu == true ):
-                echo '<li><a href="#anchor-' . $i_anchor . '"><h3 class="is-bold">' . $text_menu_name . '</h3></a></li>';
-              endif;
-              $i_anchor++;
-
-            elseif( get_row_layout() == 'grid_primary' ):
-              $gridPri_menu = get_sub_field( 'gridPri_o_menu' );
-              $gridPri_menu_name = get_sub_field( 'gridPri_o_menu_name' );
-              if( $gridPri_menu == true ):
-                echo '<li><a href="#anchor-' . $i_anchor . '"><h3 class="is-bold">' . $gridPri_menu_name . '</h3></a></li>';
+                echo '<li><a href="' . $text_link . '"><h3 class="is-bold">' . $text_menu_name . '</h3></a></li>';
               endif;
               $i_anchor++;
 
             elseif( get_row_layout() == 'grid_secondary' ):
               $gridSec_menu = get_sub_field( 'gridSec_o_menu' );
               $gridSec_menu_name = get_sub_field( 'gridSec_o_menu_name' );
+              ( !is_front_page() ) ? $gridSec_link = home_url() . '/#anchor-' . $i_anchor : $gridSec_link = home_url() . '/#anchor-' . $i_anchor;
               if( $gridSec_menu == true ):
-                echo '<li><a href="#anchor-' . $i_anchor . '"><h3 class="is-bold">' . $gridSec_menu_name . '</h3></a></li>';
+                echo '<li><a href="' . $gridSec_link . '"><h3 class="is-bold">' . $gridSec_menu_name . '</h3></a></li>';
               endif;
               $i_anchor++;
-
-            elseif( get_row_layout() == 'slider' ):
-              $slider_menu = get_sub_field( 'slider_o_menu' );
-              $slider_menu_name = get_sub_field( 'slider_o_menu_name' );
-              if( $slider_menu == true ):
-                echo '<li><a href="#anchor-' . $i_anchor . '"><h3 class="is-bold">' . $slider_menu_name . '</h3></a></li>';
-              endif;
-              $i_anchor++;
-
             endif;
 
           endwhile;
@@ -48,7 +35,7 @@
     endif;
     ?>
 
-    <li><a href="#footer"><h3 class="is-bold">Contact</h3></a></li>
+    <li><a href="<?php echo home_url(); ?>/#footer"><h3 class="is-bold">Contact</h3></a></li>
     <li><a href="<?php echo home_url(); ?>/news"><h3 class="is-bold">News</h3></a></li>
   </ul>
 </nav>
