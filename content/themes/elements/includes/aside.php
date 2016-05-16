26d529f216672882dc2e5db7f9ff55d6ade50e69
Luc Awater <lucawater@gmail.com>
2016-05-16 15:39:33 +0200

    Style aside content

--- content/themes/elements/includes/aside.php
+++ content/themes/elements/includes/aside.php
@@ -3,10 +3,10 @@
   <?php
   $categories = get_categories();
   ?>
-  <p>Categories:</p>
   <nav>
+    <p>Categories:</p>
     <?php foreach($categories as $cat): ?>
-      <li><a href="<?php echo get_category_link( $cat->term_id ); ?>"><?php echo $cat->name; ?></a></li>
+      <li><a href="<?php echo get_category_link( $cat->term_id ); ?>"><strong><?php echo $cat->name; ?></strong></a></li>
     <?php endforeach; ?>
   </nav>
 
@@ -26,7 +26,7 @@
         <?php $category = get_the_category(); ?>
         <li>
           <p><strong><?php echo $category[0]->cat_name; ?>:</strong> <?php the_title(); ?></p>
-          <p><?php echo get_the_date(); ?></p>
+          <time><img src="<?php echo get_template_directory_uri(); ?>/img/icon-time.svg" /><p><?php echo get_the_date(); ?></p></time>
         </li>
 
       <?php endwhile; ?>
