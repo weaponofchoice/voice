d803af4c213a0ce81d478356986bdacf13e9d4c4
Luc Awater <lucawater@gmail.com>
2016-05-16 11:51:51 +0200

    Add posts previews to sidebar

--- content/themes/elements/includes/nav.php
+++ content/themes/elements/includes/nav.php
@@ -4,34 +4,30 @@
     $homepage_ID = get_option( 'page_on_front' );
 
     if( have_posts() ):
-      while( have_posts() ): the_post();
+      if( have_rows('page', $homepage_ID) ): $i_anchor = 1;
+        while( have_rows('page', $homepage_ID) ): the_row();
 
-        if( have_rows('page', $homepage_ID) ): $i_anchor = 1;
-          while( have_rows('page', $homepage_ID) ): the_row();
-
-            if( get_row_layout() == 'text' ):
-              $text_menu = get_sub_field( 'text_o_menu' );
-              $text_menu_name = get_sub_field( 'text_o_menu_name' );
-              ( !is_front_page() ) ? $text_link = home_url() . '/#anchor-' . $i_anchor : $text_link = home_url() . '/#anchor-' . $i_anchor;
-              if( $text_menu == true ):
-                echo '<li><a href="' . $text_link . '"><h3 class="is-bold">' . $text_menu_name . '</h3></a></li>';
-              endif;
-              $i_anchor++;
-
-            elseif( get_row_layout() == 'grid_secondary' ):
-              $gridSec_menu = get_sub_field( 'gridSec_o_menu' );
-              $gridSec_menu_name = get_sub_field( 'gridSec_o_menu_name' );
-              ( !is_front_page() ) ? $gridSec_link = home_url() . '/#anchor-' . $i_anchor : $gridSec_link = home_url() . '/#anchor-' . $i_anchor;
-              if( $gridSec_menu == true ):
-                echo '<li><a href="' . $gridSec_link . '"><h3 class="is-bold">' . $gridSec_menu_name . '</h3></a></li>';
-              endif;
-              $i_anchor++;
+          if( get_row_layout() == 'text' ):
+            $text_menu = get_sub_field( 'text_o_menu' );
+            $text_menu_name = get_sub_field( 'text_o_menu_name' );
+            ( !is_front_page() ) ? $text_link = home_url() . '/#anchor-' . $i_anchor : $text_link = home_url() . '/#anchor-' . $i_anchor;
+            if( $text_menu == true ):
+              echo '<li><a href="' . $text_link . '"><h3 class="is-bold">' . $text_menu_name . '</h3></a></li>';
             endif;
+            $i_anchor++;
 
-          endwhile;
-        endif;
+          elseif( get_row_layout() == 'grid_secondary' ):
+            $gridSec_menu = get_sub_field( 'gridSec_o_menu' );
+            $gridSec_menu_name = get_sub_field( 'gridSec_o_menu_name' );
+            ( !is_front_page() ) ? $gridSec_link = home_url() . '/#anchor-' . $i_anchor : $gridSec_link = home_url() . '/#anchor-' . $i_anchor;
+            if( $gridSec_menu == true ):
+              echo '<li><a href="' . $gridSec_link . '"><h3 class="is-bold">' . $gridSec_menu_name . '</h3></a></li>';
+            endif;
+            $i_anchor++;
+          endif;
 
-      endwhile;
+        endwhile;
+      endif;
     endif;
     ?>
 
