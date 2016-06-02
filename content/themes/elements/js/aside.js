fd0692966cf177dd535c9c9ad3f9d8f2a0557bae
Luc Awater <lucawater@gmail.com>
2016-06-02 19:41:16 +0200

    IE Fix: main height and post width
    Main tag needed a display:block. Article in blog overview needed width:100%.

--- content/themes/elements/js/aside.js
+++ content/themes/elements/js/aside.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
   var aside = $('aside');
 
   window.onscroll = function() {
-    if( $('body').scrollTop() > 149 ){
+    if( $('body').scrollTop() > 200 ){
       aside.addClass("is-fixed");
     } else {
       aside.removeClass("is-fixed");
