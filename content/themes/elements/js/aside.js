bf6d25370bb70f80f57f60e7ce02e6c627efa633
Luc Awater <lucawater@gmail.com>
2016-06-02 19:51:26 +0200

    IE Fix: fallsafe for scrollTop

--- content/themes/elements/js/aside.js
+++ content/themes/elements/js/aside.js
@@ -2,8 +2,24 @@ $(document).ready( function() {
 
   var aside = $('aside');
 
+  function getScrollTop(){
+    if(typeof pageYOffset!= 'undefined'){
+        //most browsers except IE before #9
+        return pageYOffset;
+    }
+    else{
+        var B= document.body; //IE 'quirks'
+        var D= document.documentElement; //IE with doctype
+        D= (D.clientHeight)? D: B;
+        return D.scrollTop;
+    }
+  }
+
+
   window.onscroll = function() {
-    if( document.body.scrollTop > 200 ){
+    var scrollPos = getScrollTop();
+
+    if( scrollPos > 200 ){
       aside.addClass("is-fixed");
     } else {
       aside.removeClass("is-fixed");
