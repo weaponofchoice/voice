b3e36e9c63087383b053e2135866964cf4c2f331
Luc Awater <lucawater@gmail.com>
2016-06-02 19:48:01 +0200

    IE Fix: aside fixed class
    IE couldn't deal with scrollTop on a jquery declared element. Replaced with document.body.scrollTop.

--- content/themes/elements/js/aside.js
+++ content/themes/elements/js/aside.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
   var aside = $('aside');
 
   window.onscroll = function() {
-    if( $('body').scrollTop() > 200 ){
+    if( document.body.scrollTop > 200 ){
       aside.addClass("is-fixed");
     } else {
       aside.removeClass("is-fixed");
